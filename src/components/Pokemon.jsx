10238ee15d9ca87d94f6a25e9ba352fc448f9b0a
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-09-05 16:39:25 -0400

    Pokemon API practice

---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const Pokemon = (props) => {
+
+  const [pokemonList, setPokemonList] = useState([])
+
+  const getPokemon = () => {
+
+
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+    .then((response) => {return response.json();})
+    .then((response) => {setPokemonList(response.results);})
+    .catch((err) => {
+      console.log(err);
+    });
+
+  }
+
+  return (
+    <div className="container text-center mt-5">
+      <button className="btn btn-lg btn-success" onClick={getPokemon}>
+        Fetch Pokemon
+      </button>
+      <ul className="mt-5 list-unstyled">
+        {pokemonList.length > 0 && 
+        pokemonList.map((pokemon,index) => {
+          return <li key={index}>{index+1}-{pokemon.name}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Pokemon
